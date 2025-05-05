f3b66d63f12bbf4a96e7851bceb9d35f885738e0
yeryy30 <yermadege@alu.edu.gva.es>
2025-05-05 22:38:35 +0200

    Add files via upload

---
+++ Memoria DAM/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Text;
+internal class Program
+{
+    static void LeerFichero()
+    {
+        StreamReader sr = new StreamReader("Fichero.txt", Encoding.UTF8, true); 
+        while(!sr.EndOfStream)
+            Console.WriteLine(sr.ReadLine());
+        sr.Close();      
+
+    }
+
+    static void EscribirFichero()
+    {
+        do
+        {
+            Console.WriteLine("Pulsa Esc para dejar de escribir.");
+            Console.WriteLine("Introduce el termino que quieres escribir: ");
+            string termino = Console.ReadLine() ?? "";
+
+            Console.WriteLine("Introduce la definicion del termino: ");
+            string definicion = Console.ReadLine() ?? "";
+
+            using StreamWriter sw = new StreamWriter("Fichero.txt", true, Encoding.UTF8);
+            sw.WriteLine(termino + ": " + definicion);
+            sw.Close();
+        } while (Console.ReadKey().Key != ConsoleKey.Escape);
+    }
+    private static void Main(string[] args)
+    {
+        LeerFichero();
+
+    }
+}
